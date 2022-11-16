a9a30243d9a71f3cfdf58c9f5f3f9b5e2635b30e
Ali Shata <alishata128@gmail.com>
2022-11-16 15:47:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,41 +8,41 @@ const bodyParser = require("body-parser");
 
 // Start up an instance of app
 
-const app = express();
+const appInstance = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+appInstance.use(bodyParser.urlencoded({ extended: false }));
+appInstance.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require("cors");
-app.use(cors());
+appInstance.use(cors());
 
 // Initialize the main project folder
-app.use(express.static("website"));
+appInstance.use(express.static("website"));
 
 const data = [];
-app.post("/add", addInfo);
+appInstance.post("/add", addData);
 
-function addInfo(req, res) {
+function addData(req, res) {
   projectData["date"] = req.body.date;
   projectData["temp"] = req.body.temp;
   projectData["content"] = req.body.content;
   res.send(projectData);
 }
 
-app.get("/all", getInfo);
+appInstance.get("/all", getData);
 
-function getInfo(req, res) {
+function getData(req, res) {
   res.send(projectData);
 }
 
 // Setup Server
 
 const PORT = 3000;
-const server = app.listen(PORT, listening);
+const server = appInstance.listen(PORT, listener);
 
-function listening() {
+function listener() {
   console.log(`server is running now on port ${PORT}`);
 }
